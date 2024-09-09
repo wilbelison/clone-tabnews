0f71d3e6246bb4ea0b0f0b7c2666e65a63fda1c5
Wilbelison Junior <meustudio@gmail.com>
2024-09-09 20:53:07 +0000

    adiciona arquivo .editorconfig

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Lilian, eu amo você ❤️ Se você tem um amigo que vende churrasquinho, da uma risadinha!</h1>;
+  return (
+    <h1>
+      Lilian, eu amo você ❤️ Se você tem um amigo que vende churrasquinho, da
+      uma risadinha!
+    </h1>
+  );
 }
 
 export default Home;
